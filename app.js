483bce5dcd4f3f3dc8a4a3980f055a8a38b3ba28
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-09-23 12:10:38 +0600

    functionality done for feni

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const donateSection = document.getElementById("donate-section");
 const historySection = document.getElementById("history-section");
 const inputNoakhali = document.getElementById("input-noakhali");
 const donateNoakhali = document.getElementById("donate-noakhali");
-const totalAmount = parseFloat(
-  document.getElementById("total-amount").innerText
-);
+let totalAmount = parseFloat(document.getElementById("total-amount").innerText);
 let updatedAmountEl = parseFloat(
   document.getElementById("updated-amount").innerText
 );
 let sum = 0;
+const donateFeni = document.getElementById("donate-feni");
+const inputFeni = document.getElementById("input-feni");
 
 // historyBtn
 historyBtn.addEventListener("click", function () {
@@ -46,7 +46,8 @@ donateNoakhali.addEventListener("click", function () {
     return alert("You don't have enough money");
   }
 
-  const currentAmount = totalAmount - inputNoakhaliValue;
+  let currentAmount = totalAmount - inputNoakhaliValue;
+  totalAmount = currentAmount;
   document.getElementById("total-amount").innerText = currentAmount;
 
   inputNoakhali.value = "";
@@ -63,6 +64,45 @@ donateNoakhali.addEventListener("click", function () {
   historyDiv.innerHTML += `
     <h1 class="text-xl font-bold">
                   ${updatedAmount} Taka is Donated for Flood at Noakhali, Bangladesh.
+    </h1>
+    <p class="text-secondary font-light">
+        Date : ${new Date()}
+    </p>
+  `;
+  historyDesc.insertBefore(historyDiv, historyDesc.firstChild);
+});
+
+// donateFeni
+donateFeni.addEventListener("click", function () {
+  const inputFeniValue = parseFloat(
+    document.getElementById("input-feni").value
+  );
+  // condition
+  if (inputFeniValue <= 0 || isNaN(inputFeniValue)) {
+    return alert("Invalid amount.");
+  } else if (inputFeniValue > totalAmount) {
+    return alert("You don't have enough money");
+  }
+  // currentAmount
+  let currentAmount = totalAmount - inputFeniValue;
+  totalAmount = currentAmount;
+  document.getElementById("total-amount").innerText = currentAmount;
+  // reset input field
+  inputFeni.value = "";
+  // updatedAmount
+  const updatedAmountFeni = document.getElementById("updated-amount-feni");
+  let updatedAmountFeniValue = parseFloat(updatedAmountFeni.innerText);
+  let updatedAmount = updatedAmountFeniValue + inputFeniValue;
+  updatedAmountFeniValue = updatedAmount;
+  updatedAmountFeni.innerText = updatedAmount;
+  // hideInitialHistory
+  document.getElementById("historyHide").classList.remove("hidden");
+  // historyDesc
+  const historyDesc = document.getElementById("history-desc");
+  const historyDiv = document.createElement("div");
+  historyDiv.innerHTML += `
+    <h1 class="text-xl font-bold">
+                  ${inputFeniValue} Taka is Donated for Flood Relief in Feni,Bangladesh.
     </h1>
     <p class="text-secondary font-light">
         Date : ${new Date()}
