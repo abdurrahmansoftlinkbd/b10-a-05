1bb7f1fd2eed17cce121be032589565824c04604
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-09-23 09:14:59 +0600

    functionality added for donationBtn

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ const donateSection = document.getElementById("donate-section");
 const historySection = document.getElementById("history-section");
 
 // historyBtn
-historyBtn.addEventListener("click", function (e) {
-  historyBtn.classList.add("bg-lime-300", "border-lime-300");
+historyBtn.addEventListener("click", function () {
+  historyBtnClassAdd("bg-lime-300", "border-lime-300");
   historyBtn.classList.remove("bg-transparent");
   donationBtn.classList.remove("bg-lime-300", "border-lime-300");
   donationBtn.classList.add("bg-transparent", "border-gray1");
   donateSection.classList.add("hidden");
   historySection.classList.remove("hidden");
 });
+// historyBtn functionality
+function historyBtnClassAdd(bg, border) {
+  historyBtn.classList.add(bg, border);
+}
 
 // donationBtn
-donationBtn.addEventListener("click", function (e) {
+donationBtn.addEventListener("click", function () {
+  donationBtn.classList.add("bg-lime-300", "border-lime-300");
+  donationBtn.classList.remove("bg-transparent");
+  historyBtn.classList.remove("bg-lime-300", "border-lime-300");
+  historyBtnClassAdd("bg-transparent", "border-gray1");
   historySection.classList.add("hidden");
   donateSection.classList.remove("hidden");
 });
